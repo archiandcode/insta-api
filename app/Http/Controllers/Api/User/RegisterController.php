5e045ff46043da3ab1670e5250d20b582e9f0de4
Arshidin <osm.arshidin@gmail.com>
2024-07-21 21:50:37 +0500

    move RegisterController to Api namespace

--- app/Http/Controllers/Api/User/RegisterController.php
+++ app/Http/Controllers/Api/User/RegisterController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\User;
+namespace App\Http\Controllers\Api\User;
 
 use App\Facades\User;
 use App\Http\Controllers\Controller;
--- routes/groups/v1/current_user.php
+++ routes/groups/v1/current_user.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\User\RegisterController;
+use App\Http\Controllers\Api\User\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'user', 'as' => 'user.'], function() {
