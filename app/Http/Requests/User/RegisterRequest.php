86a5f95d876832db4984df48933d717f94910a7a
Arshidin <osm.arshidin@gmail.com>
2024-07-21 21:46:49 +0500

    create validation rules for user registration

---
+++ app/Http/Requests/User/RegisterRequest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Requests\User;
+
+use App\Http\Requests\ApiRequest;
+
+class RegisterRequest extends ApiRequest
+{
+    public function rules(): array
+    {
+        return [
+            "name" => "required|string|max:255",
+            "login" => "required|string|max:255|unique:users,login",
+            "email" => "required|string|max:255|email|unique:users,email",
+            "password" => "required|string|confirmed|min:8",
+        ];
+    }
+}
