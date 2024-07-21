a89d68377e269d86693d654236606042c90211cb
Arshidin <osm.arshidin@gmail.com>
2024-07-21 21:47:15 +0500

    add RegisterController

---
+++ app/Http/Controllers/User/RegisterController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Facades\User;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\User\RegisterRequest;
+
+class RegisterController extends Controller
+{
+    public function __invoke(RegisterRequest $request)
+    {
+        $user = User::store($request->validated());
+
+        return response()->json([
+            'id' => $user->id
+        ], 201);
+    }
+}
