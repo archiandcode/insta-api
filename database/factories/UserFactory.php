68788ca1544dab85c5b154694e13c82d80ec326f
Arshidin <osm.arshidin@gmail.com>
2024-07-21 21:38:39 +0500

    Create factories

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,7 +3,6 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 /**
@@ -11,11 +10,6 @@ use Illuminate\Support\Str;
  */
 class UserFactory extends Factory
 {
-    /**
-     * The current password being used by the factory.
-     */
-    protected static ?string $password;
-
     /**
      * Define the model's default state.
      *
@@ -25,9 +19,12 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
+            'login' => fake()->unique()->userName(),
             'email' => fake()->unique()->safeEmail(),
+            'avatar' => fake()->imageUrl,
+            'is_verified' => fake()->boolean,
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ];
     }
