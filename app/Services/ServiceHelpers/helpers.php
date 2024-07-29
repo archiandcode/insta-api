b4c9cc12dad2df409e74517ad58efbe6c7ad90d1
Arshidin <osm.arshidin@gmail.com>
2024-07-29 21:16:53 +0500

    add upload image helper function

--- app/Services/ServiceHelpers/helpers.php
+++ app/Services/ServiceHelpers/helpers.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\UploadedFile;
 use Illuminate\Http\JsonResponse;
 
 function responseFailed(string $message = null, int $code = 400): JsonResponse
@@ -7,4 +8,12 @@ function responseFailed(string $message = null, int $code = 400): JsonResponse
     return response()->json([
         'message' => $message ?? _('Bad request')
     ], $code);
+}
+
+
+function uploadImage(UploadedFile $image): string
+{
+    $path = $image->storePublicly('avatars');
+
+    return config('app.url')."/storage/$path";
 }
