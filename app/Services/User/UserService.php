b0642e8c01906fbc077449191666d4274e16948c
Arshidin <osm.arshidin@gmail.com>
2024-07-29 21:12:40 +0500

    add new methods

--- app/Services/User/UserService.php
+++ app/Services/User/UserService.php
@@ -2,26 +2,29 @@
 
 namespace App\Services\User;
 
+use App\Data\User\LoginData;
+use App\Data\User\RegisterData;
+use App\Data\User\UpdateUserData;
+use App\Http\Resources\User\CurrentUserResource;
 use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Hash;
 
 class UserService
 {
-    public function store(array $data): User
+    public function store(RegisterData $data): CurrentUserResource
     {
-        return User::query()->create([
-            'name' => $data['name'],
-            "login" => $data['login'],
-            "email" => $data['email'],
-            "password" => $data['password'],
-        ]);
+        return new CurrentUserResource(
+            User::query()->create($data->toArray())
+        );
     }
 
-    public function login(array $data) {
-        $user = User::query()->where('email', $data['email'])->first();
+    public function login(LoginData $data): JsonResponse {
+        $user = User::query()->where('email', $data->email)->first();
 
 
-        if ($user && Hash::check($data['password'], $user->password)) {
+        if ($user && Hash::check($data->password, $user->password)) {
             $user->tokens()->delete();
 
             $token = $user->createToken('auth_token')->plainTextToken;
@@ -30,6 +33,25 @@ class UserService
                 'token' => $token
             ]);
         }
+
         return responseFailed('Invalid credentials', 401);
+    }
+
+    public function update(UpdateUserData $data):User {
+        auth()->user()->update($data->toArray());
+
+        /* @var User */
+        return auth()->user();
+    }
+
+    public function updateAvatar(UploadedFile $avatar) {
+        /** @var User $user*/
+        $user = auth()->user();
+
+        $user->update([
+            'avatar' => uploadImage($avatar)
+        ]);
+
+        return $user;
     }
 }
