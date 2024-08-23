e43a92e37a915ec35b01d3ec68fab9fce8addc2b
Arshidin <osm.arshidin@gmail.com>
2024-08-23 22:29:19 +0500

    update

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,33 @@
 
 namespace Tests;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Laravel\Sanctum\Sanctum;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    private User $user;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->withHeader('Accept','application/json');
+
+        $this->user = User::factory()->create(['avatar' => null]);
+
+        Sanctum::actingAs($this->user);
+    }
+
+    public function getUser(): User
+    {
+        return $this->user;
+    }
+
+    public function getUserId(): int {
+        return $this->user->id;
+    }
 }
