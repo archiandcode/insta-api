4863cb07c5fe299ce761e4e78dd249c6b72643cd
Arshidin <osm.arshidin@gmail.com>
2024-08-24 22:44:02 +0500

    update php doc

--- app/Facades/Post.php
+++ app/Facades/Post.php
@@ -8,15 +8,15 @@ use App\Services\Post\PostService;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @method static \Illuminate\Database\Eloquent\Model store(\App\Http\Requests\Post\PostStoreRequest $request)
+ * @method static \Illuminate\Database\Eloquent\Model store(\App\Data\Post\CreatePostData $data)
  * @method static \App\Http\Resources\Post\PostResource|\Illuminate\Http\JsonResponse update(\App\Models\Post $post, \App\Data\Post\UpdatePostData $data)
  * @method static \Illuminate\Http\JsonResponse|\Illuminate\Http\Response delete(\App\Models\Post $post)
- *
  * @see PostService;
  */
+
 class Post extends Facade
 {
-    public static function getFacadeAccessor()
+    public static function getFacadeAccessor(): string
     {
         return PostService::class;
     }
--- app/Facades/User.php
+++ app/Facades/User.php
@@ -7,16 +7,19 @@ use Illuminate\Support\Facades\Facade;
 
 
 /**
- * @method static \App\Models\User store(App\Data\User\RegisterData $data)
- * @method static \Illuminate\Http\JsonResponse login(App\Data\User\LoginData $data)
- * @method static \App\Models\User update(App\Data\User\UpdateUserData $data)
- * @method static \App\Models\User updateAvatar(Illuminate\Http\UploadedFile $avatar)
+ * @method static \App\Http\Resources\User\CurrentUserResource store(\App\Data\User\RegisterData $data)
+ * @method static \Illuminate\Http\JsonResponse login(\App\Data\User\LoginData $data)
+ * @method static \App\Models\User update(\App\Data\User\UpdateUserData $data)
+ * @method static \App\Models\User updateAvatar(\Illuminate\Http\UploadedFile $avatar)
+ * @method static \Illuminate\Database\Eloquent\Collection posts(\App\Models\User $user)
+ * @method static \Illuminate\Database\Eloquent\Collection currentUserPosts()
  *
  * @see UserService;
  */
+
 class User extends Facade
 {
-    public static function getFacadeAccessor()
+    public static function getFacadeAccessor(): string
     {
         return UserService::class;
     }
